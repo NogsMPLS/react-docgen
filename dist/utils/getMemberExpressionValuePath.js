439dde98975cb61485cd3068f8d39de594c78deb
Nathan Smith <natsmith@digitalriver.com>
2016-03-14 15:41:17 -0500

    adding dist so can use branch in npm

---
+++ dist/utils/getMemberExpressionValuePath.js
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * 
+ *
+ */
+
+'use strict';
+
+var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+exports['default'] = getMemberExpressionValuePath;
+
+var _getNameOrValue = require('./getNameOrValue');
+
+var _getNameOrValue2 = _interopRequireDefault(_getNameOrValue);
+
+var _recast = require('recast');
+
+var _recast2 = _interopRequireDefault(_recast);
+
+var _resolveName = require('./resolveName');
+
+var _resolveName2 = _interopRequireDefault(_resolveName);
+
+var types = _recast2['default'].types.namedTypes;
+
+function getRoot(node) {
+  var root = node.parent;
+  while (root.parent) {
+    root = root.parent;
+  }
+  return root;
+}
+
+function getMemberExpressionValuePath(variableDefinition, memberName) {
+  var localName = (0, _resolveName2['default'])(variableDefinition);
+  var program = getRoot(variableDefinition);
+
+  if (!localName) {
+    // likely an immediately exported and therefore nameless/anonymous node
+    // passed in
+    return;
+  }
+
+  var result;
+  _recast2['default'].visit(program, {
+    visitAssignmentExpression: function visitAssignmentExpression(path) {
+      var memberPath = path.get('left');
+      if (!types.MemberExpression.check(memberPath.node)) {
+        return this.traverse(path);
+      }
+
+      if ((!memberPath.node.computed || types.Literal.check(memberPath.node.property)) && (0, _getNameOrValue2['default'])(memberPath.get('property')) === memberName) {
+        result = path.get('right');
+        return false;
+      }
+
+      this.traverse(memberPath);
+    }
+  });
+
+  return result; // eslint-disable-line consistent-return
+}
+
+module.exports = exports['default'];
